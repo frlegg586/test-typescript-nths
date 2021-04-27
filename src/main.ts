686b935d2cd1ba67e0a548f1315c1672b5a9ce64
frlegg586 <72584075+frlegg586@users.noreply.github.com>
2021-04-27 18:38:36 +1000

    Update code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-console.log("Hello, World")
-function change() {
-    document.getElementById("startTitle")!.innerHTML = "Destroy New Town High"
+initiateStartPage();
+
+function initiateStartPage() {
+    document.getElementById("currentPage")!.innerHTML = `
+    <div class="startPage">
+        <h1>Explore New Town High</h1>
+        <div class="paddedButton">
+            <button onclick="startExploring()" type="startButton" class="button">Start Exploring</button>
+        </div>
+    </div>
+    `;
 }
+
+function startExploring() {
+    document.getElementById("currentPage")!.innerHTML = `
+    <div class="explorePage">
+        
+    </div>
+    `;
+}
+  
