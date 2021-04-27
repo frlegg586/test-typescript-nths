e3b282425bd0a0acce539be6fe9f0a873bb86e19
frlegg586 <72584075+frlegg586@users.noreply.github.com>
2021-04-27 18:45:21 +1000

    Update code
    Soz, ignore the previous edit proposal, this is the real deal

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-console.log("Hello, World")
-function change() {
-    document.getElementById("startTitle")!.innerHTML = "Destroy New Town High"
+initStartPage();
+
+function initStartPage() {
+    document.getElementById("body")!.innerHTML = `
+    <div class="startPage">
+      <div class="constrainedStartPage">
+        <h1 id="startTitle">Explore New Town High</h1>
+        <div class="startPagePadding"></div>
+        <div class="paddedButton">
+          <button onclick="startExploring()" type="startButton" class="button">Start Exploring</button>
+        </div>
+      </div>
+    </div>
+    `;
+}
+
+function startExploring() {
+    document.getElementById("body")!.innerHTML = `
+
+    `;
 }
